8c390ed14116bad19bfab199a861d5baf38811b1
temple7447 <starukido@gmail.com>
2023-02-04 21:53:57 +0100

    hello

--- Models/DailyDevotionalGuide.js
+++ Models/DailyDevotionalGuide.js
@@ -3,14 +3,16 @@ const Schema = mongoose.Schema();
 
 const DailyDevotion = new Schema({
     title:{
-
+        type:String,
+        required:true
     },
     content:{
-
+        type:String,
+        required:true
     },
     createdAt:{
         Date: new Date().toLocaleString(),
     }
 })
 
-module.exports = mongoose.models('GKS', DailyDevotion);
+module.exports = mongoose.model('GKS', DailyDevotion);

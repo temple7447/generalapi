56828740163615abe79021fb4a61328c8ce5ec26
temple7447 <starukido@gmail.com>
2023-03-07 12:29:55 +0100

    hrllo

--- component/BibleQuiz.js
+++ component/BibleQuiz.js
@@ -1,16 +1,16 @@
 const BibleQuiz = [
     {
         key:1,
-        Categories:"The SonShip of Jesus Chirst",
+        Categories:"CITIES OF REFUGE",
         content:`THE  Rather, He commanded Moses, saying, “Make thee a fiery serpent, and set it upon a pole: and it shall come to pass, that every one that is bitten, when he looketh upon it, shall live.” – Numbers 21: 8.
          In obedience to the Lord’s instruction, Moses made a serpent of brass and erected it on a pole, “and it came to pass, that if a serpent had bitten any man, when he beheld the serpent of brass, he lived.”- Numbers 21: 9.`,
         BibleQuiz:[
           {
             key:1,
-            explaination: "Because is Create the Heaven and Earth according Gen 2 vs 3",
-            Question: "What do we Understand by the SonShip of Jesus Chirst one",
+            explaination: `Judgment of God (the truth) as expounded by the anointed servants or their companions and the ecclesiastical courts in God’s organization (Psalm 149:5-9; John 5:22,27).   To declare one’s cause is to take one’s stand on the side of God and repent and walk in harmony  with God – Isaiah 26:9, Psalm 119:9- 11.`,
+            Question: `Briefly explain what is meant by judgment by the “congregation” and “declaring one’s cause” in the sermon “Cities of Refuge”.`,
             options:[
-                {answer:"one one", isCorrect:true, key:1},
+                {answer: `Judgment of God (the truth) as expounded by the anointed servants or their companions and the ecclesiastical courts in God’s organization (Psalm 149:5-9; John 5:22,27).   To declare one’s cause is to take one’s stand on the side of God and repent and walk in harmony  with God – Isaiah 26:9, Psalm 119:9- 11.`, isCorrect:true, key:1},
                 {answer:"two one", isCorrect:false, key:2},
                 {answer:"three one", isCorrect:false, key:3},
                 {answer:"four one", isCorrect:false, key:4},
@@ -19,16 +19,96 @@ const BibleQuiz = [
           },
           {
             key:2,
+            explaination: `i) Jesus Christ - Jeremiah 51:20-21.
+            ii) Ignorant sinners
+            iii) God’s Kingdom now set up in this last days -
+            Matthew 24:15,16; Hebrews 11:8-10.
+             iv) End of the Priestly work on earth.`,
+            Question: `In the sermon on Cities of Refuge what is the Significance of the following: i) The Avenger of Blood ii) The Manslayer iii) The cities of refuge iv) The death of the high priest.`,
+            options:[
+                {answer:" ", isCorrect:false, key:1},
+                {answer:"Jesus Christ,", isCorrect:false, key:2},
+                {answer:" Jesus Christ , Ignorant sinners, God’s Kingdom now set up in this last days, End of the Priestly work on earth", isCorrect:true, key:3},
+                {answer:" Jesus Christ, ", isCorrect:false, key:4},
+            ]
+
+          },
+          {
+            key:3,
             explaination: "Because is Create the Heaven and Earth",
-            Question: "What do we Understand bysd the SonShip of Jesus Chirst two",
+            Question: `The cities of Refuge were cities which God
+            Almighty commanded the Jews to set aside for the benefit of the “manslayer” among the Jews as well as the strangers and sojourners dwelling among them who kills unintentionally so that he may not be overtaken by the strict law of retribution in force at that time.`,
             options:[
                 {answer:" SonShip one two", isCorrect:false, key:1},
-                {answer:" SonShip two two", isCorrect:false, key:2},
-                {answer:" SonShip three two", isCorrect:true, key:3},
+                {answer:`The cities of Refuge were cities which God Almighty commanded the Jews to set aside for the benefit of the “manslayer” among the Jews as well as the strangers and sojourners dwelling among them who kills unintentionally so that he may not be overtaken by the strict law of retribution in force at that time.`, isCorrect:true, key:2},
+                {answer:" SonShip three two", isCorrect:false, key:3},
                 {answer:" SonShip four two", isCorrect:false, key:4},
             ]
 
           },
+          {
+            key:4,
+            explaination: `There were six cities of Refuge, three on both sides of Jordan. –Numbers 35:9-34.`,
+            Question: `How many where they and where were they located?`,
+            options:[
+                {answer:" SonShip one two", isCorrect:false, key:1},
+                {answer:" SonShip two two", isCorrect:false, key:2},
+                {answer:`There were six cities of Refuge, three on both sides of Jordan. –Numbers 35:9-34.`, isCorrect:true, key:3},
+                {answer:" SonShip four two", isCorrect:false, key:4},
+            ]
+
+          },
+          {
+            key:5,
+            explaination: "It shows that this provision of God was made for the entire human race for their own protection and blessing. – Matthew 24:16; Isaiah 2:2,3.",
+            Question: `What is the significance of the fact that the city of refuge was not meant only for the natural Jews but also for strangers?`,
+            options:[
+                {answer:" SonShip one two", isCorrect:false, key:1},
+                {answer:"It shows that this provision of God was made for the entire human race for their own protection and blessing. – Matthew 24:16; Isaiah 2:2,3.", isCorrect:true, key:2},
+                {answer:" SonShip three two", isCorrect:false, key:3},
+                {answer:" SonShip four two", isCorrect:false, key:4},
+            ]
+
+          },
+          {
+            key:6,
+            explaination:``,
+            Question: `In the subject the Cities of Refuge, what roles are played antitypically by Jesus Christ and his body members`,
+            options:[
+                {answer:`They are the elders who judge the ignorant manslayer. - John 5:22,27; Psalm 149:5-9.`, isCorrect:true, key:1},
+                {answer:`sd`, isCorrect:false, key:2},
+                {answer:`sds`, isCorrect:false, key:3},
+                {answer:`none of the above`, isCorrect:false, key:4},
+                {answer:`all of the above`, isCorrect:false, key:4},
+            ]
+
+          },
+          // {
+          //   key:6,
+          //   explaination:``,
+          //   Question: ``,
+          //   options:[
+          //       {answer:`city one`, isCorrect:false, key:1},
+          //       {answer:``, isCorrect:true, key:2},
+          //       {answer:``, isCorrect:false, key:3},
+          //       {answer:`none of the above`, isCorrect:false, key:4},
+          //       {answer:`all of the above`, isCorrect:false, key:4},
+          //   ]
+
+          // },
+          // {
+          //   key:6,
+          //   explaination:``,
+          //   Question: ``,
+          //   options:[
+          //       {answer:`city one`, isCorrect:false, key:1},
+          //       {answer:``, isCorrect:true, key:2},
+          //       {answer:``, isCorrect:false, key:3},
+          //       {answer:`none of the above`, isCorrect:false, key:4},
+          //       {answer:`all of the above`, isCorrect:false, key:4},
+          //   ]
+
+          // },
         ]
 
     },

f6f7c8ba2e37681c68c11e8a99d6e9e0da253423
temple7447 <starukido@gmail.com>
2023-05-04 18:13:00 +0100

    sdf

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require('cors')
 const SermonDetails = require('./component/Sermons')
 const axios = require("axios");
 const mongoose = require('mongoose');
-const path=  require("path")
+const path = require("path")
 const multer = require('multer');
 const MusicRouter = require('./Router/Music')
 const BibleQuiz = require('./Router/BibleQuiz')
@@ -16,7 +16,7 @@ const data = require('./component/test')
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
-  
+
 
 connectDB()
 
@@ -30,34 +30,34 @@ app.use('/', DailyDevotional)
 app.use('/', BibleQuiz)
 app.use('/', MelodyRouter)
 app.use('/', MusicRouter)
-app.get('/sermon',(req,res)=>{
-res.send(SermonDetails)
+app.get('/sermon', (req, res) => {
+    res.send(SermonDetails)
 })
 
 
-app.get("/musicm", (req,res)=>{
-    res.send("the name is test and what can i give you in return")
+app.get("/musicm", (req, res) => {
+    res.send("the name is test and whdfd at can i give you in return")
 })
 
-app.get('/state',(req,res)=>{
+app.get('/state', (req, res) => {
     res.send(State)
-    
+
 })
 
 
-app.get('/testing',(req,res)=>{
+app.get('/testing', (req, res) => {
     axios.get('https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users')
-.then((response)=>{
-    res.send(response.data[0])
-})
-.catch((err)=>{ console.log(err)})
+        .then((response) => {
+            res.send(response.data[0])
+        })
+        .catch((err) => { console.log(err) })
 
 })
 
-app.get('*',(req,res)=>{
+app.get('*', (req, res) => {
     res.send('page was not found')
 })
 const PORT = process.env.PORT || 4000;
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
 })
